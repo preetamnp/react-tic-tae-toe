be523fd8433aef5242599919ccd1828b181187cc
preetampotadar <preetam.potadar@cognizant.com>
2025-05-16 21:30:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
+import {useState} from "react";
+import Log from "./components/Log.jsx";
+import {WINNING_COMBINATIONS} from "./winning-combinations.js";
+import GameOver from "./components/GameOver.jsx";
+
+const PLAYERS = {
+    X: 'Player 1',
+    O: 'Player 2'
+};
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+
+function deriveActivePlayer(gameTurns) {
+    let currentPlayer = 'X';
+
+    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+        currentPlayer = 'O';
+    }
+    return currentPlayer;
+}
+function App() {
+    const [players, setPlayers] = useState(PLAYERS)
+    const [gameTurns, setGameTurns] = useState([]);
+    const activePlayer = deriveActivePlayer(gameTurns);
+    let gameBoard = [...initialGameBoard].map((row) => [...row]);
+    let winner;
+    for (const turn of gameTurns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+    for(const combination of WINNING_COMBINATIONS) {
+        let firstCombination = gameBoard[combination[0].row][combination[0].column];
+        let secondCombination = gameBoard[combination[1].row][combination[1].column];
+        let thirdCombination = gameBoard[combination[2].row][combination[2].column];
+        if(firstCombination && firstCombination === secondCombination && firstCombination === thirdCombination) {
+            winner = players[firstCombination];
+        }
+
+    }
+
+
+    const hasDraw = gameTurns.length === 9 && !winner;
+
+    console.log(winner + "or " + hasDraw)
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameTurns((prevTurns) => {
+
+            const currentPlayer = deriveActivePlayer(prevTurns);
+            const updatedTurns = [
+                { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+                ...prevTurns,
+            ];
+            return updatedTurns;
+        });
+    }
+
+    function handleRestart() {
+        setGameTurns([]);
+    }
+
+    function handleChangePlayerName(symbol, name) {
+        setPlayers((prevPlayers) => {
+            return {
+                ...prevPlayers,
+                [symbol]: name
+            }
+        })
+
+    }
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players" className="highlight-player">
+                    <Player initialName="Player1" onChangeName={handleChangePlayerName} playerSymbol="X" isActive={activePlayer === 'X'}/>
+                    <Player initialName="Player2" onChangeName={handleChangePlayerName} playerSymbol="O" isActive={activePlayer === 'O'}/>
+                </ol>
+                { (winner || hasDraw) && <GameOver onRestart={handleRestart} winner={winner} />}
+                <GameBoard board={gameBoard} onSelectSquare={handleSelectSquare} />
+            </div>
+            <Log turns={gameTurns}/>
+        </main>
+    )
+}
+
+export default App
---
+++ src/components/Player.jsx
@@ -0,0 +1,29 @@
+import {useState} from "react";
+
+export default function Player({initialName, playerSymbol, isActive}, onChangeName) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
+    function handleOnClick() {
+        setIsEditing((isEditing) => !isEditing);
+        if(isEditing) {
+            onChangeName(playerSymbol, playerName);
+        }
+    }
+    function handlePlayerChange(e) {
+        setPlayerName(e.target.value);
+    }
+    let editablePlayerTag = <span className="player-name">{playerName}</span>;
+    if(isEditing) {
+        editablePlayerTag = <input type="text" value={playerName}
+                                    onChange={handlePlayerChange} />
+    }
+    return (
+        <li className={isActive ? "active" : undefined}>
+              <span id="player">
+                {editablePlayerTag}
+                <span className="player-symbol">{playerSymbol}</span>
+              </span>
+            <button onClick={handleOnClick} >{isEditing ? "Save" : "Edit" }</button>
+        </li>
+    );
+}
